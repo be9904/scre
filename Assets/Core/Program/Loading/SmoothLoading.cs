f2b9c4bc305ae3a64e293a06be41268b81262a58
Junghoon Choi <be9904@gmail.com>
2023-02-16 04:12:35 +0900

    add radial progress bar

---
+++ Assets/Core/Program/Loading/SmoothLoading.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SmoothLoading : MonoBehaviour
+{
+    private AsyncOperation loadOperation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
