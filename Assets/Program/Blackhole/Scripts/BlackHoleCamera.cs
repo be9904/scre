b7291c257fdc130a15420777913b84409206f743
Junghoon Choi <be9904@gmail.com>
2023-02-17 08:04:15 +0900

    finish black hole

--- Assets/Program/Blackhole/Scripts/BlackHoleCamera.cs
+++ Assets/Program/Blackhole/Scripts/BlackHoleCamera.cs
@@ -1,6 +1,9 @@
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
 
+[RequireComponent(typeof(UIDocument))]
 public class BlackHoleCamera : MonoBehaviour
 {
     [Header("Settings")] [SerializeField] private Material blackHoleMaterial;
@@ -25,12 +28,35 @@ public class BlackHoleCamera : MonoBehaviour
 
     private Vector4 bounds;
 
+    private UIDocument      programUI;
+    private Label           title;
+    private Label           description;
+    private Slider          radiusSlider;
+    private Slider          discRadiusSlider;
+    private Slider          rotationSensitivitySlider;
+    private Slider          scrollSensitivitySlider;
+    private Button          returnButton;
+
+    [Header("UI Elements")]
+    [SerializeField] private SText titleText;
+    [SerializeField] private SText descriptionText;
+
 #if UNITY_EDITOR
     private void Reset()
     {
         camera = GetComponent<Camera>();
     }
 #endif
+
+    private void OnEnable()
+    {
+        BindUIElements();
+    }
+
+    private void OnDisable()
+    {
+        programUI.enabled = false;
+    }
 
     private void Start()
     {
@@ -92,5 +118,49 @@ public class BlackHoleCamera : MonoBehaviour
         blackHoleMaterial.SetVector(positionId, camera.WorldToViewportPoint(targetPosition));
         blackHoleMaterial.SetFloat(distanceId,
             (Vector3.Dot(thisToTarget, transform.forward) > 0 ? 1 : -1) * thisToTarget.magnitude);
+    }
+
+    void BindUIElements()
+    {
+        programUI = GetComponent<UIDocument>();
+        var root = programUI.rootVisualElement;
+        
+        title = root.Q<Label>("Title");
+        description = root.Q<Label>("Description");
+        radiusSlider = root.Q<Slider>("radius");
+        discRadiusSlider = root.Q<Slider>("disc-radius");
+        rotationSensitivitySlider = root.Q<Slider>("rotate");
+        scrollSensitivitySlider = root.Q<Slider>("scroll");
+        returnButton = programUI.rootVisualElement.Q<Button>("Return");
+        
+        // set text
+        title.text = titleText.Value;
+        description.text = descriptionText.Value;
+        
+        // register events
+        radiusSlider.RegisterCallback<ChangeEvent<float>>(evt =>
+        {
+            target.radius = evt.newValue;
+        });
+        
+        discRadiusSlider.RegisterCallback<ChangeEvent<float>>(evt =>
+        {
+            target.discRadius = evt.newValue;
+        });
+        
+        rotationSensitivitySlider.RegisterCallback<ChangeEvent<float>>(evt =>
+        {
+            panSensitivity = evt.newValue;
+        });
+        
+        scrollSensitivitySlider.RegisterCallback<ChangeEvent<float>>(evt =>
+        {
+            scrollSensitivity = evt.newValue;
+        });
+
+        returnButton.RegisterCallback<ClickEvent>(evt =>
+        {
+            ProgramUtility.ReturnToMain();
+        });
     }
 }
