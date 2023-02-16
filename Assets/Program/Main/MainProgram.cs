03ed36004a9b7e2b1da8d929b03c3b64a02a113c
Junghoon Choi <be9904@gmail.com>
2023-02-17 05:31:02 +0900

    update main ui

--- Assets/Program/Main/MainProgram.cs
+++ Assets/Program/Main/MainProgram.cs
@@ -11,13 +11,14 @@ public class MainProgram : MonoBehaviour
     [SerializeField] private FullScreenBlurFeature blurFeature;
     [SerializeField] private GlitchFeature glitchFeature;
     [SerializeField] private SInt mainBlurStrength;
-    
-    
-    
+
     // UI Elements
     private UIDocument mainUI;
     private Label about;
     private Label members;
+    private List<Button> shaders;
+    private List<Button> textures;
+    private List<Button> srp;
 
     [SerializeField] private SText mainAbout;
     [SerializeField] private SText mainMembers;
@@ -42,6 +43,36 @@ public class MainProgram : MonoBehaviour
         mainUI = GetComponent<UIDocument>();
         about = mainUI.rootVisualElement.Q<Label>("about-field");
         members = mainUI.rootVisualElement.Q<Label>("members-field");
+        
+        // mainUI.rootVisualElement.Q<Button>("shaders-01-button").RegisterCallback<ClickEvent>(evt =>
+        // {
+        //     SmoothLoading.SetNextSceneIndex(2);
+        // });
+        // mainUI.rootVisualElement.Q<Button>("shaders-02-button").RegisterCallback<ClickEvent>(evt =>
+        // {
+        //     SmoothLoading.SetNextSceneIndex(3);
+        // });
+        // mainUI.rootVisualElement.Q<Button>("shaders-03-button").RegisterCallback<ClickEvent>(evt =>
+        // {
+        //     SmoothLoading.SetNextSceneIndex(4);
+        //     
+        // });
+        
+        mainUI.rootVisualElement.Q<Button>("textures-01-button").RegisterCallback<ClickEvent>(evt =>
+        {  
+            SmoothLoading.SetNextSceneIndex(2);
+            SceneManager.LoadScene(1);
+        });
+        mainUI.rootVisualElement.Q<Button>("textures-02-button").RegisterCallback<ClickEvent>(evt =>
+        {  
+            SmoothLoading.SetNextSceneIndex(3);
+            SceneManager.LoadScene(1);
+        });
+        mainUI.rootVisualElement.Q<Button>("srp-01-button").RegisterCallback<ClickEvent>(evt =>
+        {  
+            SmoothLoading.SetNextSceneIndex(4);
+            SceneManager.LoadScene(1);
+        });
 
         about.text = mainAbout.Value;
         members.text = mainMembers.Value;
