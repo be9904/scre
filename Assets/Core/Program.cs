35b75ca36679218a2b040704fa3a5c9bb5539637
Junghoon Choi <be9904@gmail.com>
2023-02-21 17:45:07 +0900

    create root program class

---
+++ Assets/Core/Program.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(UIDocument))]
+public class Program : MonoBehaviour
+{
+    // UI Elements
+    protected UIDocument      programUI;
+    protected VisualElement   rootVE;
+    protected Label           title;
+    protected Label           description;
+    protected Button          returnButton;
+    
+    [Header("UI Text Fields")]
+    [SerializeField] private SText titleText;
+    [SerializeField] private SText descriptionText;
+    
+    [Header("Scriptable Renderer Feature")]
+    [SerializeField] protected ScriptableRendererFeature rendererFeature;
+
+    private void OnEnable()
+    {
+        // get main component and root ve
+        programUI = GetComponent<UIDocument>();
+        rootVE = programUI.rootVisualElement;
+
+        // queue UI elements
+        title = rootVE.Q<Label>("Title");
+        description = rootVE.Q<Label>("Description");
+        returnButton = rootVE.Q<Button>("Return");
+        
+        // set text
+        title.text = titleText.Value;
+        description.text = descriptionText.Value;
+        
+        // register callback event
+        returnButton.RegisterCallback<ClickEvent>(evt =>
+        {
+            ProgramUtility.ReturnToMain();
+        });
+    }
+}
